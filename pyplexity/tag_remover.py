2e28b1fcdeda249ca1c7b5db14f50cffd3ec30ea
Marcos Fernández Pichel <marcosfernandez.pichel@usc.es>
2022-03-01 17:46:07 +0100

    requirements.txt + LICENSE

--- pyplexity/tag_remover.py
+++ pyplexity/tag_remover.py
@@ -1,3 +1,19 @@
+# PyPlexity
+# Copyright (C) 2022 Manuel Prada Corral
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import lxml
 from lxml import etree
 from lxml.html import HtmlElement, html5parser
@@ -12,9 +28,6 @@ class HTMLTagRemover(ContentProcessor):
 
     def __init__(self):
         self.build_text_list = etree.XPath("//text()")
-        # logging.getLogger("bs4").setLevel(logging.CRITICAL)
-        # logging.getLogger("bs4.dammit").setLevel(logging.CRITICAL)
-        # warnings.simplefilter("ignore")  # disable htmlsoup warnings
 
     def _remove_html_tags_lxml(self, content):
         content = content.decode(errors="ignore").encode("ascii", errors="ignore")
@@ -23,22 +36,7 @@ class HTMLTagRemover(ContentProcessor):
         except etree.ParserError:
             return ""
         except:  # if lxml (faster parser) doesnt work, try with html5lib (slower but more stable)
-            print("html5lib")
             html = html5parser.fromstring(content)
-        texts = self.build_text_list(html)  # recorrido eficiente de textos con eliminacion posterior: 6.05s
+        texts = self.build_text_list(html)
         res = " ".join([s.strip() for s in texts if s.getparent().tag not in ["script", "style"]])
         return res
-        # recorrido eficiente con eliminacion eficiente: 6.21s
-        # for bad in html.xpath("//style|//script"):
-        #    bad.getparent().remove(bad)
-        # return etree.tostring(html, method="text", encoding="utf8")
-
-    # def _remove_html_tags_bs4(self, content):
-    #     try:
-    #         soup = BeautifulSoup(content, "lxml")  # parse HTML
-    #     except:  # if lxml (faster parser) doesnt work, try with html5lib (slower but more stable)
-    #         print("html5lib")
-    #         soup = BeautifulSoup(content, "html5lib")
-    #     for script in soup(["script", "style"]):  # tag removal from parsed HTML tree
-    #         script.decompose()
-    #     return soup.get_text(separator=" ", strip=True)  # extract text from rest of HTML tags
